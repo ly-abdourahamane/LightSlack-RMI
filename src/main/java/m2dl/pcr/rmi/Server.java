377e2a78ed4b3a8b34fcbc6f895c8c449a46e965
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-04-14 16:44:15 +0200

    Create server and implement Hello interface

---
+++ src/main/java/m2dl/pcr/rmi/Server.java
@@ -0,0 +1,33 @@
+package m2dl.pcr.rmi;
+// rmiregistry -J-Djava.rmi.server.codebase=file:/home/abd-dell/Documents/M2DL/PCR/TP/target/classes/
+// -Djava.rmi.server.codebase=file:/home/abd-dell/Documents/M2DL/PCR/TP/target/classes/
+// java  -classpath /home/abd-dell/Documents/M2DL/PCR/TP/target/classes/ m2dl.pcr.rmi.Client
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server  implements Hello {
+
+    private Server() {}
+
+    public static void main(String args[]) {
+
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public String sayHello() {
+        return "Hello, world!";
+    }
+}
